c204163267e380d1308610f4b7d5d15d13c5c72a
aditrustandi <31118385+aditrustandi@users.noreply.github.com>
2019-12-19 21:25:31 +0700

    Update main.js

--- ttt-native/js/main.js
+++ ttt-native/js/main.js
@@ -13,7 +13,7 @@ $('#go').click(
         val_x = [];
         $('#versus_key').val(versus)
         count = 0;
-        if (val > 2 && val < 11) {
+        if (val > 2) {
             $('#wrapper').html('');
             $('#alert').fadeOut(300)
             for (let index = 0; index < val; index++) {
@@ -40,7 +40,7 @@ $('#go').click(
             
                 
         }else{
-            $('#alert').html("Board scale must be 1 to 10")
+            $('#alert').html("Board scale must be greather than 3")
             $('#alert').fadeIn(300)
         }
         
@@ -53,8 +53,9 @@ function btnBoxClicked(v)
 
     if (el.html() === '~') {
         count = count+1;
+        let res = [];
 
-        if (isP2(count, v)) {
+        if (isP2(count)) {
             
             var p = p2,
                 style = 'warn';
@@ -65,7 +66,7 @@ function btnBoxClicked(v)
                 
         }
     
-        if (isP1(count, v)) {
+        if (isP1(count)) {
             var p = p1,
                 style = 'skyblue';
 
@@ -82,21 +83,12 @@ function btnBoxClicked(v)
         }
         
         let horizontalC = checkHorizontalCenter(p, v),
-            horizontalL = checkHorizontalLeft(p, v),
-            horizontalR = checkHorizontalRight(p, v),
 
             verticalC = checkVerticalCenter(p, v),
-            verticalT = checkVerticalTop(p, v),
-            verticalB = checkVerticalBottom(p, v),
 
+            traverseFromLeftC = checkTraverseFromLeftCenter(p),
 
-            traverseFromLeftC = checkTraverseFromLeftCenter(p, v),
-            traverseFromLeftT = checkTraverseFromLeftTop(p, v),
-            traverseFromLeftB = checkTraverseFromLeftBottom(p, v),
-
-            traverseFromRightC = checkTraverseFromRightCenter(p, v),
-            traverseFromRightT = checkTraverseFromRightTop(p, v),
-            traverseFromRightB = checkTraverseFromRightBottom(p, v);
+            traverseFromRightC = checkTraverseFromRightCenter(p)
 
         var btn_box = document.getElementsByClassName('btn-box'),
             checkingDraw = [];
@@ -109,38 +101,37 @@ function btnBoxClicked(v)
             
         }
 
-        if (checkingDraw.includes('~') === false) {
-            $('#alert').html("Draw !")
+        res = [
+            horizontalC,
+            verticalC,
+            traverseFromLeftC,
+            traverseFromRightC
+        ]
+
+        
+        if (checkingResult(res) !== false) {
+            
+            if (checkingResult(res).length > 1) {
+                $('#alert').html(p+" win with "+checkingResult(res).length+" combos !")
+            }else{
+                $('#alert').html(p+" win !")
+            }
+
             $('#alert').fadeIn(300)
 
             for (let index = 0; index < btn_box.length; index++) {
                 btn_box[index].disabled = true;
             }
-        }
-        
+        }else{
+            if (checkingDraw.includes('~') === false) {
+                $('#alert').html("Draw !")
+                $('#alert').fadeIn(300)
 
-        if (horizontalC === true || 
-            horizontalL === true || 
-            horizontalR === true || 
-            verticalB === true || 
-            verticalC === true || 
-            verticalT === true || 
-            traverseFromLeftC === true ||
-            traverseFromLeftT === true ||
-            traverseFromLeftB === true ||
-            traverseFromRightC === true ||
-            traverseFromRightT === true ||
-            traverseFromRightB === true) {
-
-            $('#alert').html(p+" win !")
-            $('#alert').fadeIn(300)
-
-            for (let index = 0; index < btn_box.length; index++) {
-                btn_box[index].disabled = true;
+                for (let index = 0; index < btn_box.length; index++) {
+                    btn_box[index].disabled = true;
+                }
             }
         }
-        
-        
         
         
     }
@@ -149,11 +140,11 @@ function btnBoxClicked(v)
 
 
 
-function isP2(n, v) {
+function isP2(n) {
     return n % 2 == 0;
  }
  
- function isP1(n, v) {
+ function isP1(n) {
     return Math.abs(n % 2) == 1;
  }
 
@@ -177,352 +168,118 @@ function isP2(n, v) {
         btnBoxClicked(v)
  }
 
- function checkTraverseFromRightBottom(p, v)
+ function checkingResult(arr)
  {
-    let firstDigit = parseInt(v.charAt(0)),
-        lastDigit = parseInt(v.charAt(1)),
-        maxNum = parseInt($('.btn-box:last').attr('c'));
+     if (arr.includes(true) === true) {
+        let finalResult = removeA(arr, false);
 
-    if (firstDigit-2 < 0) {
-        return false
-    }else if(lastDigit+2 > maxNum){
-        return false
-    } else{
-        let centerBoxRightLD = firstDigit-1,
-            centerBoxRightRD = lastDigit+1,
-            centerBoxRightID = ""+centerBoxRightLD+""+centerBoxRightRD+"";
-
-        let centerBoxRightLDNext = firstDigit-2,
-            centerBoxRightRDNext = lastDigit+2,
-            centerBoxRightIDNext = ""+centerBoxRightLDNext+""+centerBoxRightRDNext+"";
-
-        if ($('#'+centerBoxRightID).hasClass(p) === $('#'+centerBoxRightIDNext).hasClass(p) && $('#'+v).hasClass(p) === $('#'+centerBoxRightID).hasClass(p) && $('#'+centerBoxRightIDNext).hasClass(p) === $('#'+v).hasClass(p)) {
-            return true;
-        }else{
-            return false;
-        } 
-    }
+        return finalResult
+     }else{
+         return false
+     }
  }
 
- function checkTraverseFromRightTop(p, v)
- {
-    let firstDigit = parseInt(v.charAt(0)),
-        lastDigit = parseInt(v.charAt(1)),
-        maxNum = parseInt($('.btn-box:last').attr('c'));
+function removeA(arr) {
+    var what, a = arguments, L = a.length, ax;
+    while (L > 1 && arr.length) {
 
-        if (firstDigit+2 > maxNum) {
-            return false
-        }else if(lastDigit-2 < 0)
-        {
-            return false
-        }else{
-
-            let centerBoxLeftLD = firstDigit+1,
-                centerBoxLeftRD = lastDigit-1,
-                centerBoxLeftID = ""+centerBoxLeftLD+""+centerBoxLeftRD+"";
-
-            let centerBoxLeftLDNext = firstDigit+2,
-                centerBoxLeftRDNext = lastDigit-2,
-                centerBoxLeftIDNext = ""+centerBoxLeftLDNext+""+centerBoxLeftRDNext+"";
-
-            if ($('#'+centerBoxLeftID).hasClass(p) === $('#'+centerBoxLeftIDNext).hasClass(p) && $('#'+v).hasClass(p) === $('#'+centerBoxLeftID).hasClass(p) && $('#'+centerBoxLeftIDNext).hasClass(p) === $('#'+v).hasClass(p)) {
-                return true;
-            }else{
-                return false;
-            }   
-
+        what = a[--L];
+        while ((ax= arr.indexOf(what)) !== -1) {
+            arr.splice(ax, 1);
         }
- }
-
- function checkTraverseFromRightCenter(p, v)
- {
-    let firstDigit = parseInt(v.charAt(0)),
-        lastDigit = parseInt(v.charAt(1)),
-        maxNum = parseInt($('.btn-box:last').attr('c'));
-
-    if (firstDigit-1 < 0) {
-        return false
-    }else if(lastDigit+1 > maxNum){
-        return false
-    }else if(firstDigit+1 > maxNum){
-        return false
-    }else if(lastDigit-1 < 0){
-        return false
-    }else{
-        
-        let topBoxRightLD = firstDigit-1,
-            topBoxRightRD = lastDigit+1,
-            topBoxRightID = ""+topBoxRightLD+""+topBoxRightRD+"";
-
-        let bottomBoxLefttLD = firstDigit+1,
-            bottomBoxLeftRD = lastDigit-1,
-            bottomBoxLeftID = ""+bottomBoxLefttLD+""+bottomBoxLeftRD+"";
-
-        
-        if ($('#'+topBoxRightID).hasClass(p) === $('#'+bottomBoxLeftID).hasClass(p) && $('#'+v).hasClass(p) === $('#'+topBoxRightID).hasClass(p) && $('#'+bottomBoxLeftID).hasClass(p) === $('#'+v).hasClass(p)) {
-            return true;
-        }else{
-            return false;
-        }     
 
     }
- }
 
- function checkTraverseFromLeftBottom(p, v)
- {
-    let firstDigit = parseInt(v.charAt(0)),
-        lastDigit = parseInt(v.charAt(1));
+    return arr;
+}
 
-        if (firstDigit-2 < 0) {
-            return false
-        }else if(lastDigit-2 < 0){
-            return false
-        }else{
-            let topBoxLeftLD = firstDigit-1,
-                topBoxLeftRD = lastDigit-1,
-                topBoxLeftID = ""+topBoxLeftLD+""+topBoxLeftRD+"";
 
-            let topBoxLeftLDNext = firstDigit-2,
-                topBoxLeftRDNext = lastDigit-2,
-                topBoxLeftIDNext = ""+topBoxLeftLDNext+""+topBoxLeftRDNext+"";
-
-            if ($('#'+topBoxLeftID).hasClass(p) === $('#'+topBoxLeftIDNext).hasClass(p) && $('#'+v).hasClass(p) === $('#'+topBoxLeftID).hasClass(p) && $('#'+topBoxLeftIDNext).hasClass(p) === $('#'+v).hasClass(p)) {
-                return true;
-            }else{
-                return false;
-            }
-        }
- }
-
- function checkTraverseFromLeftTop(p, v)
- {
-    let firstDigit = parseInt(v.charAt(0)),
-        lastDigit = parseInt(v.charAt(1)),
-        maxNum = parseInt($('.btn-box:last').attr('c'));
-
-    if (firstDigit+2 > maxNum) {
-        return false
-    }else{
-        let centerBoxRightLD = firstDigit+1,
-            centerBoxRightRD = lastDigit+1,
-            centerBoxRighID = ""+centerBoxRightLD+""+centerBoxRightRD+"";
-        
-        let bottomBoxRightLD = firstDigit+2,
-            bottomBoxRightRD = lastDigit+2,
-            bottomBoxRightID = ""+bottomBoxRightLD+""+bottomBoxRightRD+"";
-
-        if ($('#'+centerBoxRighID).hasClass(p) === $('#'+bottomBoxRightID).hasClass(p) && $('#'+v).hasClass(p) === $('#'+centerBoxRighID).hasClass(p) && $('#'+bottomBoxRightID).hasClass(p) === $('#'+v).hasClass(p)) {
-            return true;
-        }else{
-            return false;
-        }
-    }
- }
-
-function checkTraverseFromLeftCenter(p, v)
+function checkTraverseFromLeftCenter(p)
 {
-    let firstDigit = parseInt(v.charAt(0)),
-        lastDigit = parseInt(v.charAt(1)),
-        maxNum = parseInt($('.btn-box:last').attr('c'));
+    let minNum = parseInt($('.btn-box:first').attr('c')),
+        val = $('#dimensi-box').val(),
+        res = [];
 
-    if (firstDigit-1 < 0) {
-        return false;
-    }else if(lastDigit-1 < 0){
-        return false;
-    }else if(firstDigit+1 > maxNum){
-        return false;
-    }else if(lastDigit+1 > maxNum){
-        return false;
-    } else{
-        
-        let topBoxLeftLD = firstDigit-1,
-            topBoxLeftRD = lastDigit-1,
-            topBoxLeftID = ""+topBoxLeftLD+""+topBoxLeftRD+"";
 
-        let btmBoxRightLD = firstDigit+1,
-            btmBoxRightRD = lastDigit+1,
-            btmBoxRightID = ""+btmBoxRightLD+""+btmBoxRightRD+"";
+        for (let index = 0; index < val; index++) {
+            let d = minNum+index;
 
-        if ($('#'+topBoxLeftID).hasClass(p) === $('#'+btmBoxRightID).hasClass(p) && $('#'+v).hasClass(p) === $('#'+topBoxLeftID).hasClass(p) && $('#'+btmBoxRightID).hasClass(p) === $('#'+v).hasClass(p)) {
+                checkBox = $('#'+d+""+d+"");
+
+                res.push(checkBox.hasClass(p))
+            
+        }
+
+        if (res.includes(false) === false) {
             return true;
         }else{
-            return false; 
+            return false;
         }
-    }
+
     
 }
 
- function checkVerticalBottom(p, v)
+function checkTraverseFromRightCenter(p)
  {
-    let firstDigit = parseInt(v.charAt(0)),
-        lastDigit = parseInt(v.charAt(1));
+    let minNum = parseInt($('.btn-box:first').attr('c')),
+        maxNum = parseInt($('.btn-box:last').attr('c')),
+        val = $('#dimensi-box').val(),
+        res = [];
 
-    if (firstDigit-2 < 0) {
-        return false
-    }else{
-        
-        let topBoxRD = firstDigit-1,
-            topBoxID = ""+topBoxRD+""+lastDigit+"";
 
-        let topBoxRDNext = firstDigit-2,
-            topBoxIDNext = ""+topBoxRDNext+""+lastDigit+"";
+        for (let index = 0; index < val; index++) {
+            let fd = minNum+index,
+                ld = maxNum-index,
+                checkBox = $('#'+fd+""+ld+"");
+
+                res.push(checkBox.hasClass(p))
             
-       
-        if ($('#'+topBoxID).hasClass(p) === $('#'+topBoxIDNext).hasClass(p) && $('#'+v).hasClass(p) === $('#'+topBoxID).hasClass(p) && $('#'+topBoxIDNext).hasClass(p) === $('#'+v).hasClass(p)) {
-            return true;
-        }else{
-            return false; 
         }
 
-    }
- }
-
- function checkVerticalTop(p, v)
- {
-    let firstDigit = parseInt(v.charAt(0)),
-        lastDigit = parseInt(v.charAt(1)),
-        maxNum = parseInt($('.btn-box:last').attr('c'));
-
-        if (firstDigit+2 > maxNum) {
-            return false;
+        if (res.includes(false) === false) {
+            return true;
         }else{
-            let btmBoxLD = firstDigit+1,
-                btmBoxID = ""+btmBoxLD+""+lastDigit+"";
-            
-            let btmBoxLDNext = firstDigit+2,
-                btmBoxIDNext = ""+btmBoxLDNext+""+lastDigit+"";
-
-            
-            if ($('#'+btmBoxIDNext).hasClass(p) === $('#'+btmBoxID).hasClass(p) && $('#'+v).hasClass(p) === $('#'+btmBoxID).hasClass(p) && $('#'+btmBoxIDNext).hasClass(p) === $('#'+v).hasClass(p)) {
-                return true;
-            }else{
-                return false; 
-            }
-        };
+            return false;
+        }
  }
 
  function checkVerticalCenter(p, v)
  {
-    let firstDigit = parseInt(v.charAt(0)),
-        lastDigit = parseInt(v.charAt(1)),
-        maxNum = parseInt($('.btn-box:last').attr('c'));
+    let lastDigit = parseInt(v.charAt(1)),
+        val = $('#dimensi-box').val(),
+        res = [];
 
-    if (firstDigit-1 < 0) {
-        return false;
-    } else if(firstDigit === maxNum){
-        return false;
-    }else{
+        for (let index = 0; index < val; index++) {
+            let checkBox = $('#'+index+""+lastDigit+"")
 
-        let topBoxRD = firstDigit-1,
-            topBoxID = ""+topBoxRD+""+lastDigit+"";
-
-        let btmBoxRD = firstDigit+1,
-            btmBoxID = ""+btmBoxRD+""+lastDigit+"";
-
-            if ($('#'+topBoxID).hasClass(p) === $('#'+btmBoxID).hasClass(p) && $('#'+v).hasClass(p) === $('#'+btmBoxID).hasClass(p) && $('#'+topBoxID).hasClass(p) === $('#'+v).hasClass(p)) {
-                return true;
-            }else{
-                return false; 
-            }
-    }
- }
-
- function checkHorizontalRight(p, v)
- {
-    let firstDigit = parseInt(v.charAt(0)),
-        lastDigit = parseInt(v.charAt(1)),
-        minNum = parseInt($('.btn-box:first').attr('c'));
-
-    if (lastDigit === minNum) {
-        return false
-    }else if(lastDigit-2 < minNum){
-        return false;
-    }else{
-        
-        let leftBoxLD = lastDigit-1,
-            leftBoxID = ""+firstDigit+""+leftBoxLD+"";
-
-        let leftBoxLDNext = lastDigit-2,
-            leftBoxIDNext = ""+firstDigit+""+leftBoxLDNext+"";
-
-
-        if ($('#'+leftBoxID).html() === '~' && $('#'+leftBoxIDNext).html() === '~') {
-            return false;
-        }else{ 
-
-            if ($('#'+leftBoxID).hasClass(p) === $('#'+leftBoxIDNext).hasClass(p) && $('#'+v).hasClass(p) === $('#'+leftBoxIDNext).hasClass(p) && $('#'+leftBoxID).hasClass(p) === $('#'+v).hasClass(p)) {
-                return true;
-            }else{
-                return false;
-            }
-
-        }
-
-    }
- }
-
- function checkHorizontalLeft(p, v)
- {
-    let firstDigit = parseInt(v.charAt(0)),
-        lastDigit = parseInt(v.charAt(1)),
-        maxNum = parseInt($('.btn-box:last').attr('c'));
-
-        if (lastDigit === maxNum) {
-            return false
-        }else if(lastDigit+2 > maxNum){
-            return false
-        }else{
+            res.push(checkBox.hasClass(p))
             
-            let rightBoxLD = lastDigit+1,
-                rightBoxID = ""+firstDigit+""+rightBoxLD+"";
-
-            let rightBoxLDNext = lastDigit+2,
-                rightBoxIDNext = ""+firstDigit+""+rightBoxLDNext+"";
-
-
-            if ($('#'+rightBoxID).html() === '~' && $('#'+rightBoxIDNext).html() === '~') {
-                return false;
-            }else{ 
-
-                if ($('#'+rightBoxID).hasClass(p) === $('#'+rightBoxIDNext).hasClass(p) && $('#'+v).hasClass(p) === $('#'+rightBoxIDNext).hasClass(p) && $('#'+rightBoxID).hasClass(p) === $('#'+v).hasClass(p)) {
-                    return true;
-                }else{
-                    return false;
-                }
-
-            }
+        }
+        
+        if (res.includes(false) === false) {
+            return true;
+        }else{
+            return false;
         }
  }
+
 
  function checkHorizontalCenter(p, v)
  {
     let firstDigit = parseInt(v.charAt(0)),
-        lastDigit = parseInt(v.charAt(1)),
-        maxNum = parseInt($('.btn-box:last').attr('c'));
+        val = $('#dimensi-box').val(),
+        res = [];
 
-        if (lastDigit-1 < 0) {
-            return false
-        }else if(lastDigit === maxNum){
-            return false;
+        for (let index = 0; index < val; index++) {
+            let checkBox = $('#'+firstDigit+""+index+"")
+
+            res.push(checkBox.hasClass(p))
+            
+        }
+        
+        if (res.includes(false) === false) {
+            return true;
         }else{
-            
-            let leftBoxLD = lastDigit-1,
-                leftBoxID = ""+firstDigit+""+leftBoxLD+"";
-            
-            let rightBoxLD = lastDigit+1,
-                rightBoxID = ""+firstDigit+""+rightBoxLD+"";
-
-            if ($('#'+rightBoxID).html() === '~' && $('#'+leftBoxID).html() === '~') {
-                return false;
-            }else{
-                
-                if ($('#'+rightBoxID).hasClass(p) === $('#'+leftBoxID).hasClass(p) && $('#'+v).hasClass(p) === $('#'+leftBoxID).hasClass(p) && $('#'+rightBoxID).hasClass(p) === $('#'+v).hasClass(p)) {
-                    return true;
-                }else{
-                    return false;
-                }
-
-            }
-
+            return false;
         }
  }
